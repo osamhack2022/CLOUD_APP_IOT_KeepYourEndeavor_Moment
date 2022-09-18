019c3363e980c9f1e858346ef99b987bd4d32133
eunsolkang <eunsol2953@gmail.com>
2022-09-18 18:01:20 +0900

    feature: Add Kafka

---
+++ CLOUD(BE)/src/kafka/initKafka.ts
@@ -0,0 +1,30 @@
+import { Kafka } from "kafkajs";
+
+export default async function (ky2: any) {
+    //TODO: Config File로 대체예정
+    const kafkaClient = new Kafka({
+        clientId: '',
+        brokers: ['localhost:29092']
+    });
+
+    const admin = kafkaClient.admin()
+    await admin.connect()
+    await admin.createTopics({
+        waitForLeaders: true,
+        topics: [
+          { topic: 'test-topic' },
+        ],
+    })
+
+    const producer = kafkaClient.producer()
+    const consumer = kafkaClient.consumer({ groupId: 'test-group' })
+    await consumer.connect()
+    await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            await ky2.onMessage(topic, message.value);
+        }
+    });
+
+    return {producer, consumer};
+}
