431e9731f0b4d8bb95870b2ac2a831cd44aa3e42
eunsolkang <eunsol2953@gmail.com>
2022-09-18 18:03:47 +0900

    feature: GUI Console

---
+++ CLOUD(BE)/console/server/app.ts
@@ -0,0 +1,20 @@
+//TODO: API 서버 Console로 변경 예정
+import express from 'express';
+import routes from './routes';
+
+const server = async(config, sdk, db) => {
+    const app = express();
+
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+
+    app.use((req:any, res, next) => {
+        req.db = db;
+        req.sdk = sdk;
+        return next();
+    });
+
+    app.use("/v1", routes);
+    
+    app.listen(config.port);
+}
