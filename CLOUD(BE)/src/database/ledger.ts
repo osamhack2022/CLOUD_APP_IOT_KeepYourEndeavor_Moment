c72d86ea7486c01b792aa0aa1c840e7796067c52
eunsolkang <eunsol2953@gmail.com>
2022-10-06 23:11:07 +0900

    feature: Add Query using userID #62

--- CLOUD(BE)/src/database/ledger.ts
+++ CLOUD(BE)/src/database/ledger.ts
@@ -1,4 +1,4 @@
-import { Ottoman, start, close } from "ottoman";
+import { Ottoman, start, close, Query } from "ottoman";
 import { Block } from "../block";
 import { BlockModel } from "./BlockModel";
 
@@ -9,10 +9,24 @@ export default class {
         this.ottoman = ottoman;
     }
 
-    async getBlock(): Promise<any>{
-        const data = await BlockModel.find();
-
-        return data; 
+    async getBlock(user?: string, offset?: number, limit?: number): Promise<any>{
+        try{
+            if(!user){
+                const data = await BlockModel.find();
+                return data;
+            }
+            const query = new Query({}, 'test-buket t')
+                            .select('t.*')
+                            .where({ '`data`.`user`': user })
+                            .offset(offset ?? 0)
+                            .limit(limit ?? 10)
+                            .build();
+            const {rows} = await this.ottoman.query(query);
+            return rows; 
+        }catch(e){
+            console.log(e);
+            
+        }
     }
 
     async getLeastBlock(): Promise<any>{
